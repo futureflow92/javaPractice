c0272b71caeb92788b248e4785bb265686ee4748
futureflow92 <futureflow92@hotmail.com>
2022-10-05 16:29:11 +0900

    Chapter8 예제 1회차 실습(23개 중 17까지) 완료

---
+++ src/main/java/com/java/javapractice/ch08_ExceptionHandling/ExceptionEx12.java
@@ -0,0 +1,15 @@
+package com.java.javapractice.ch08_ExceptionHandling;
+
+class ExceptionEx12 {
+    public static void main(String[] args) throws Exception {
+        method1(); //같은 클래스내의 static멤버이므로 객체생성없이 직접 호출 가능.
+    }
+
+    static void method1() throws Exception {
+        method2();
+    }
+
+    static void method2() throws Exception {
+        throw new Exception();
+    }
+}
